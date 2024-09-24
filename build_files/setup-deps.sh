5930495360afe24575952c0340351050188f2051
bugrovegor <bugrovegor@yandex-team.com>
2024-09-24 13:08:46 +0300

    pandora 0.5.32 for ulta commit_hash:b9140ad2a6c4edc984fa54934098dfb6abe39d99

--- build_files/setup-deps.sh
+++ build_files/setup-deps.sh
@@ -63,7 +63,7 @@ install_phantom() {
 
 install_pandora() {
     install_curl
-    PANDORA_BINARY="https://github.com/yandex/pandora/releases/download/v0.5.30/pandora_0.5.30_linux_amd64"
+    PANDORA_BINARY="https://github.com/yandex/pandora/releases/download/v0.5.32/pandora_0.5.32_linux_amd64"
     curl -fS -L --connect-time 3 --max-time 30 --retry 10 "${PANDORA_BINARY}" -o /usr/local/bin/pandora || log_fatal "Can't download pandora"
     chmod +x /usr/local/bin/pandora
     mkdir -p /usr/share/doc/pandora
